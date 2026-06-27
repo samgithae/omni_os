bc388905d0584a917113ef9277284e407fa2fb26
samgithae <samgithae01@gmail.com>
2026-06-27 09:53:55 +0300

    Pint auto-fix: routes/api.php + routes/debug.php

--- routes/debug.php
+++ routes/debug.php
@@ -1,7 +1,10 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
+
 Route::get('debug-token', function () {
     $expected = config('app.omni_api_token');
+
     return response()->json([
         'expected' => $expected,
         'expected_len' => strlen($expected),
